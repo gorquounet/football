0c2c6f0c5e2d3d1a2f874ec1bfa34132673fb513
thomashuguet <thomas.r.huguet@gmail.com>
2018-07-04 13:54:13 +0200

    formulaire en cours

--- src/fr/football/servlet/InscriptionServlet.java
+++ src/fr/football/servlet/InscriptionServlet.java
@@ -1,7 +1,6 @@
 package fr.football.servlet;
 
-
-mport java.io.IOException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -21,65 +20,53 @@ import fr.Utils.DataConnect;
 @WebServlet(name = "InscriptionServelet", urlPatterns = ("/Inscription"))
 public class InscriptionServlet  extends HttpServlet{
 
-    private static final long serialVersionUID = 1L;
-
-    private static final String CHAMP_NOM   = "nom";
-    private static final String CHAMP_PRENOM   = "prenom";
-    public static final String CHAMP_EMAIL = "email";
-    private static final String CHAMP_PASS   = "password";
-    private static final String CHAMP_NIVEAU   = "niveau";
-    private static final String CHAMP_VILLE   = "ville";
-    public static final String CHAMP_CP= "cp";
-    public static final String CHAMP_ADDRESS ="address";
-    public InscriptionServelets() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    public static final String VUE = "/WEB-INF/Inscription.jsp";
+    public static final String CHAMP_EMAIL  = "email";
+    public static final String CHAMP_PASS   = "motdepasse";
+    public static final String CHAMP_CONF   = "confirmation";
+    public static final String CHAMP_NOM    = "nom";
+    public static final String CHAMP_PRENOM = "prenom";
+    public static final String CHAMP_ADRESS = "address";
+    public static final String CHAMP_CP     = "69003";
+    public static final String CHAMP_VILLE  = "ville";
+    public static final String CHAMP_NAISS  = "23/06/1996";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        String resultat;
-        Map<String, String> erreurs = new HashMap<String, String>();
-
-        String nom = request.getParameter(CHAMP_NOM);
-        String prenom = request.getParameter(CHAMP_PRENOM);
-        String email =  request.getParameter(CHAMP_EMAIL);
-        String motDePasse = request.getParameter(CHAMP_PASS);
-        String niveau = request.getParameter(CHAMP_NIVEAU);
-        String ville = request.getParameter(CHAMP_VILLE);
-
+        /* Récupération des champs du formulaire. */
+        String email        = request.getParameter( CHAMP_EMAIL );
+        String motDePasse   = request.getParameter( CHAMP_PASS );
+        String confirmation = request.getParameter( CHAMP_CONF );
+        String nom          = request.getParameter( CHAMP_NOM );
+        String prenom       = request.getParameter( CHAMP_PRENOM);
+        String address      = request.getParameter( CHAMP_ADRESS );
+        String cp           = request.getParameter( CHAMP_CP);
+        String ville        = request.getParameter( CHAMP_VILLE );
+        String naiss        = request.getParameter( CHAMP_NAISS );
 
         try {
             validationEmail( email );
-        } catch ( Exception e ) {
-            erreurs.put( CHAMP_EMAIL, e.getMessage() );
-        }
-        if ( erreurs.isEmpty() ) {
-            inscriptionJoueur(nom,prenom,email,motDePasse,niveau,ville);
-            response.sendRedirect("/EnglishBattle/");
-        } else {
-            resultat = "�chec de l'inscription.";
-            request.setAttribute( "erreurs", erreurs );
-            request.setAttribute( "resultat", resultat );
-            doGet( request, response );
-        }
-
-        private void validationEmail( String email ) throws Exception {
-            if (!email.isEmpty()) {
-                if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
-                    throw new Exception( "Merci de saisir une adresse mail valide." );
-                }
-            } else {
-                throw new Exception( "Merci de saisir une adresse mail." );
-            }
-        }
-
-        private void inscriptionJoueur() {
-
+            validationMotsDePasse( motDePasse, confirmation );
+            validationNom( nom );
+        } catch (Exception e) {
+            /* Gérer les erreurs de validation ici. */
         }
     }
 
+    private void validationEmail( String email ) throws Exception {
+        if (email != null && email.trim().length() != 0) {
+            if (!email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" )) {
+                throw new Exception( "Merci de saisir une adresse mail valide." );
+            }
+        } else {
+            throw new Exception( "Merci de saisir une adresse mail." );
+        }
+    }
+    private void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception{}
+    private void validationNom( String nom ) throws Exception{}
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher("/WEB-INF/Inscription.jsp").forward(request, response);
+
+
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 }
